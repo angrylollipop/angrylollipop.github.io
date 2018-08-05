02d3f3d42315793d3b3ced1eac515a54ac3ef73d
angrylollipop <38570960+angrylollipop@users.noreply.github.com>
2018-08-05 15:25:27 -0500

    Added NAV scroll behavior

---
+++ js/nav-transparent.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    // Transition effect for navbar
+    $(window).scroll(function () {
+        // checks if window is scrolled more than 100px, adds/removes solid class
+        if ($(this).scrollTop() > 10) {
+            $('.navbar').addClass('solid');
+        } else {
+            $('.navbar').removeClass('solid');
+        }
+    });
+});
